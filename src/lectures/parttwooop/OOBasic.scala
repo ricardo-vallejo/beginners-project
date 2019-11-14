59004b7f382ce869c65ffe18222b4c81a96e2ba4
Ricardo <ricardovallejo@seven4n.com>
2019-11-14 18:00:37 -0500

    Object oriented basic

--- src/lectures/parttwooop/OOBasic.scala
+++ src/lectures/parttwooop/OOBasic.scala
@@ -15,6 +15,14 @@ object OOBasic extends App {
   val personDefAge: Person = new Person("Pedro")
   println(personDefAge)
 
+  val author: Writer = new Writer("Ricardo", "Vallejo", 1987)
+  val novel: Novel = new Novel("The new book", 2017, author)
+
+  println(author.fullName())
+  println(novel.authorAge())
+  println(novel.isWritingBy(author))
+  println(novel.copy(2019))
+
 }
 
 //Constructor
@@ -24,10 +32,12 @@ class Person(name: String, val age: Int) {
   println(1 + 3)
 
   //Method
-  def greet(name: String): Unit = println(s"${this.name} says: Hi, $name") // "this" use the parameter of the class when I have a parameter in the method with the same name.
+  def greet(name: String): Unit =
+    println(s"${this.name} says: Hi, $name") // "this" use the parameter of the class when I have a parameter in the method with the same name.
 
   //Overloading : Define the same method name but with different parameters
-  def greet(): Unit = println(s"Hi, I am $name") //If I don't have parameter in the method I don't have to use "this"
+  def greet(): Unit =
+    println(s"Hi, I am $name") //If I don't have parameter in the method I don't have to use "this"
 
   //Multiple constructors
   def this(name: String) = this(name, 0)
@@ -36,3 +46,37 @@ class Person(name: String, val age: Int) {
 
 //Class parameters are NOT FIELDS
 //Add VAL or VAR to a parameter to convert into a field.
+
+class Writer(firstName: String, surname: String, val year: Int) {
+  def fullName(): String = s"$firstName $surname"
+}
+
+class Novel(name: String, yearRelease: Int, author: Writer) {
+  def authorAge(): Int = yearRelease - author.year
+  def isWritingBy(author: Writer): Boolean = author == this.author
+  def copy(newYearRelease: Int): Novel = new Novel(name, newYearRelease, author)
+}
+
+class Counter(val count: Int) {
+  def increment: Counter = {
+    println("Incrementing")
+    new Counter(count + 1) //Inmutability
+  }
+
+  def decrement: Counter = {
+    println("Decrementing")
+    new Counter(count - 1)
+  }
+
+  def increment(n: Int): Counter = {
+    if (n <= 0) this
+    else increment.increment(n - 1)
+  }
+
+  def decrement(n: Int): Counter = {
+    if (n <= 0) this
+    else decrement.decrement(n - 1)
+  }
+
+  def print = println(count)
+}
